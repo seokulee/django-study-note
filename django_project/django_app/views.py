0946e09f168531c02859fdf119e492b7934547c9
Seokkyu Lee <cd1004s@gmail.com>
2024-09-06 20:09:51 +0900

    feat basic redirection

--- django_project/django_app/views.py
+++ django_project/django_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
 
 NOT_FOUND_MSG = "Not Found Topic"
 
@@ -18,6 +18,17 @@ def news_view(request, topic):
         # raise Http404("404 GENERIC ERROR") #404.html
 
 
-# dynamic view
-def add_views(request, num1, num2):
-    return HttpResponse(f'{num1} + {num2} = {num1 + num2}')
+# domain.com/django_app/0 ---> domain.com/django_app/sports
+def num_page_view(request, num_page):
+    topics_list = list(articles.keys()) # ['sports', 'finance', 'politics']
+    try:
+        topic = topics_list[num_page]
+    except IndexError:
+        return HttpResponseNotFound(NOT_FOUND_MSG)
+
+    return HttpResponseRedirect(topic)
+
+
+# # dynamic view
+# def add_views(request, num1, num2):
+#     return HttpResponse(f'{num1} + {num2} = {num1 + num2}')
