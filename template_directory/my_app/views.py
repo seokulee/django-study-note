ef192ef55e2d5f981bdd606b3a90ae5707368a46
Seokkyu Lee <cd1004s@gmail.com>
2024-09-09 14:30:05 +0900

    feat templete variables

--- template_directory/my_app/views.py
+++ template_directory/my_app/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 def example_view(request):
     # my app/templates/my_app/example.html
     return render(request, 'my_app/example.html')
+
+def variable_view(request):
+    my_var = {
+        'first_name':'Seokkyu',
+        'last_name':'Lee',
+        'some_list': [
+            1, 2, 3,
+        ],
+        'some_dict': {
+            'inside_key': 'inside_value',
+        }
+    }
+    return render(request, 'my_app/variable.html', context=my_var)
