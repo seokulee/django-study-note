5a3b3ab6a11c68f3f83490454011e07b514bfcf9
Seokkyu Lee <cd1004s@gmail.com>
2024-09-09 19:15:00 +0900

    feat template static

--- template_directory/template_directory/settings.py
+++ template_directory/template_directory/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yf@h1)qyu-h3^h+*351hx_kwv3#g+qwj!k+czi_x9#!n%duw4%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# my_app/static/my_app/django.jpg..
 STATIC_URL = 'static/'
 
 # Default primary key field type
