34874a68aeeb33d9894b9455ed395f3877ec30c9
Seokkyu Lee <cd1004s@gmail.com>
2024-09-10 17:34:11 +0900

    feat database

---
+++ database_exercise/office/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from . import models
+
+# Create your views here.
+def list_patients(request):
+    all_patients = models.Patient.objects.all()
+    context_list = {
+        'patients':all_patients
+    }
+
+    return render(request, 'office/list.html', context_list)
