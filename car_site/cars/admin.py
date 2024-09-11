ca703ff075388b3484c6dc83d67a99b5bde4d85e
Seokkyu Lee <cd1004s@gmail.com>
2024-09-11 16:45:02 +0900

    admin model Model -> ModelAdmin

--- car_site/cars/admin.py
+++ car_site/cars/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from cars.models import Car
 
 # Register your models here.
-admin.site.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Car, CarAdmin)
