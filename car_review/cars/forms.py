d77c07cdf959e896f00135fcc8da54e99bc33c7a
Seokkyu Lee <cd1004s@gmail.com>
2024-09-19 17:52:45 +0900

    modelform

--- car_review/cars/forms.py
+++ car_review/cars/forms.py
@@ -1,14 +1,21 @@
 from django import forms
+from .models import Review
+from django.forms import ModelForm
 
-class ReviewForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=100)
-    last_name = forms.CharField(label='Last Name', max_length=100)
-    email = forms.EmailField(label='Email')
-    review = forms.CharField(
-        label='Please write your review here',
-        widget=forms.Textarea(attrs={
-            'class':'myform',
-            'rows':15,
-            'cols':30
-        })
-    )
+# class ReviewForm(forms.Form):
+#     first_name = forms.CharField(label='First Name', max_length=100)
+#     last_name = forms.CharField(label='Last Name', max_length=100)
+#     email = forms.EmailField(label='Email')
+#     review = forms.CharField(
+#         label='Please write your review here',
+#         widget=forms.Textarea(attrs={
+#             'class':'myform',
+#             'rows':15,
+#             'cols':30
+#         })
+#     )
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['first_name', 'last_name', 'stars']
